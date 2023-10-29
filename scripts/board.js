dda252c44661962b88ff63673ee22d0ff9f75288
lyk336 <134726237+lyk336@users.noreply.github.com>
2023-10-29 23:17:48 +0200

    make pieces interactive

--- scripts/board.js
+++ scripts/board.js
@@ -4,38 +4,58 @@ const board = document.getElementById('board');
 const columns = document.querySelectorAll('.board__column');
 const columnIndex = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-// Find the required cell
-function FindCell(cell) {
-  this.column = columnIndex.findIndex((element) => element === cell[0]);
-  this.row = +cell[1] - 1;
-}
-// find the required piece in HTML
-function htmlFindPiece(element) {
-  let row;
-
-  const columnId = element.parentElement.parentElement.id;
-  for (let i = 0; i < 8; i++) {
-    const elem = document.getElementById(columnId).querySelectorAll('.board__cell')[i].querySelector('.piece');
-    if (elem === element) {
-      row = i;
+class ChessPiece {
+  name;
+  color;
+  id;
+  move() {
+    switch (this.name) {
     }
   }
-  return columnId[7] + (8 - row);
+  constructor(name, color, id) {
+    this.name = name;
+    this.color = color;
+    this.id = id;
+  }
 }
 
-// find the required cell in HTML
-function htmlFindCell(element) {
-  const columnId = element.parentElement.id;
-
-  // suspicious function =)
-  const getChildIndex = (node) => {
-    return (Array.prototype.indexOf.call(node.parentNode.childNodes, node) - 1) / 2;
-  };
-  return columnId[7] + (8 - getChildIndex(element));
-}
-
+// some variables for moving
 let isPieceActive = false;
-let activePiece;
+let currentTurn = 1;
+let activePieceId;
+function activeSide() {
+  if (currentTurn % 2 === 0) {
+    return 'black';
+  } else {
+    return 'white';
+  }
+}
+// function for all pieces
+function pieceFun(pieceObj) {
+  // checking if chosen piece side can be moved now
+  if (document.getElementById(pieceObj.id).dataset.color === activeSide()) {
+    // checking if one of the pieces is inactive to make it active
+    if (!isPieceActive) {
+      isPieceActive = true;
+    }
+    // defining which piece is active or making piece inactive if it was chosen second time
+    if (activePieceId === pieceObj.id) {
+      // remove an active mark
+      document.querySelector('.piece__active').remove();
+
+      isPieceActive = false;
+      activePieceId = '';
+    } else {
+      activePieceId = pieceObj.id;
+      // add an active mark
+
+      const activePieceHtml = document.createElement('div');
+      activePieceHtml.className = 'piece__active';
+      document.getElementById(pieceObj.id).parentElement.appendChild(activePieceHtml);
+    }
+    console.log(`${pieceObj.name} on ${document.getElementById(pieceObj.id).parentElement.id}`, isPieceActive, activePieceId);
+  }
+}
 
 // add piece on board
 let id = 1;
@@ -43,41 +63,77 @@ pieces.forEach((piece) => {
   Object.entries(piece.startingCell).forEach(([color, cell]) => {
     cell.forEach((cell) => {
       const pieceId = `piece${id}`;
-      const pieceHTML = `<div class="piece black king" id="${pieceId}">
-        <img src="img/pieces/${color}/${piece.name}.png" />
-      </div>`;
+      const pieceHTML = `
+        <div class="piece" id="${pieceId}" name="${piece.name}" data-color="${color}">
+          <img src="img/pieces/${color}/${piece.name}.png" />
+        </div>
+      `;
 
       // adding on board
-      const coordinates = new FindCell(cell);
-      const requiredRow = columns[coordinates.column].querySelectorAll('.board__cell');
-
-      requiredRow[7 - coordinates.row].innerHTML = pieceHTML;
+      document.getElementById(cell).innerHTML = pieceHTML;
 
       // adding event listener for clicking on piece
       document.getElementById(pieceId).addEventListener('click', () => {
-        console.log(`${piece.name} on ${htmlFindPiece(document.getElementById(pieceId))}`);
-        isPieceActive = true;
-        activePiece = htmlFindPiece(document.getElementById(pieceId));
+        // creating piece using class
+        const chessPiece = new ChessPiece(
+          document.getElementById(pieceId).getAttribute('name'),
+          document.getElementById(pieceId).dataset.color,
+          pieceId
+        );
+
+        pieceFun(chessPiece);
       });
       id++;
     });
   });
 });
 
-// add event listeners for all cells when the player selects a piece
-board.querySelectorAll('.board__cell').forEach((cell) => {
-  function eventListenerFun(event) {
-    if (cell.querySelector('.piece')) {
-    } else {
-      if (isPieceActive) {
-        console.log(`${activePiece} -> ${htmlFindCell(cell)}`);
-        board.querySelectorAll('.board__cell').forEach((cll) => {
-          cll.removeEventListener('click', eventListenerFun);
-        });
+// adding event listener for all cells
+
+document.querySelectorAll('.board__cell').forEach((cell) => {
+  cell.addEventListener('click', () => {
+    let pieceId;
+
+    const replaceChess = function () {
+      const html = document.getElementById(activePieceId).cloneNode(true);
+      cell.innerHTML = '';
+      document.getElementById(activePieceId).remove();
+      cell.appendChild(html);
+
+      document.getElementById(html.id).addEventListener('click', () => {
+        const chessPiece = new ChessPiece(
+          document.getElementById(html.id).getAttribute('name'),
+          document.getElementById(html.id).dataset.color,
+          html.id
+        );
+
+        pieceFun(chessPiece);
+      });
+
+      // remove an active mark
+      document.querySelector('.piece__active').remove();
+
+      isPieceActive = false;
+      activePieceId = null;
+      currentTurn++;
+    };
+
+    // permission to move only when one of the pieces was chosen
+    if (isPieceActive) {
+      // console.log('move', pieceId);
+      // checking if this cell contains piece
+      if (cell.contains(cell.querySelector('.piece'))) {
+        pieceId = cell.querySelector('.piece').id;
+
+        // if piece move on opponent's piece, it eat(replaces) it
+        if (document.getElementById(pieceId).dataset.color !== activeSide()) {
+          console.log('you eat this piece');
+          replaceChess();
+        }
+      } else {
+        pieceId = null;
+        replaceChess();
       }
     }
-  }
-  cell.addEventListener('click', eventListenerFun);
+  });
 });
-function pawnMovement() {}
-// creating rules of movement for all pieces;
